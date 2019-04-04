aa57ff9c4db7d534ead8789b75338d4af7300fe0
Kibian <kevcass01@yahoo.fr>
2019-04-04 23:46:30 +0200

    modifs arbre deplacement terrain

--- src/App.js
+++ src/App.js
@@ -4,34 +4,69 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props)
+    this.goTop = this.goTop.bind(this);
+    this.goBottom = this.goBottom.bind(this);
+    this.goRight = this.goRight.bind(this);
+    this.goLeft = this.goLeft.bind(this);
     this.state = {
       labyrinth: [
-        [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,1,1,0,1,1,1,0,0,1,1,0,0,0,0,0,1],
-        [1,0,1,1,0,1,1,1,0,0,0,0,1,1,0,1,0,1,0,0,1,1,1,0,1],
+        [1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,4,0,0,0,0,0,0,0,1,1,0,1,1,1,0,0,1,1,0,0,0,0,0,1],
+        [1,3,1,1,0,1,1,1,0,0,0,0,1,1,0,1,0,1,0,0,1,1,1,0,1],
         [1,0,1,0,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,1,1,0,1,0,1],
-        [1,0,1,0,1,1,0,1,0,1,0,1,1,1,0,1,0,0,0,1,1,0,1,0,1],
-        [1,0,1,0,1,1,1,1,0,1,0,0,1,1,0,1,1,1,0,1,0,0,1,0,1],
-        [1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1],
-        [1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,0,1],
-        [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,2,1,0,1,1,0,1,0,1,0,1,1,1,0,1,0,0,0,1,1,0,1,0,1],
+        [1,3,1,0,1,1,1,1,0,1,0,0,1,1,0,1,1,1,0,1,0,0,1,0,1],
+        [1,2,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1],
+        [1,4,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,0,1],
+        [1,3,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]
       ],
 
-      posX: 0,
+      posX: 1,
       posY: 0,
-      img: 'batman'
+      img: 'charBottom'
     }
   }
-  goRight() {
-    const posX = this.state.posX + 1
-    this.setState({posX})
+  goRight(event) {
+    if(event.keyCode === 39) {
+      const posX = this.state.posX + 1
+      this.setState({posX})
+      this.setState({img: "charRight"})
+    }
   }
-  goLeft() {
-    const posX = this.state.posX -1
-    this.setState({posX})
+  goLeft(event) {
+    if(event.keyCode === 37) {
+      const posX = this.state.posX -1
+      this.setState({posX})
+      this.setState({img: "charLeft"})
+    }
   }
-
+  goTop(event) {
+    if(event.keyCode === 40) {
+      const posY = this.state.posY + 1
+      this.setState({posY})
+      this.setState({img: "charBottom"})
+    }
+  }
+  goBottom(event) {
+    if(event.keyCode === 38) {
+      const posY = this.state.posY - 1
+      this.setState({posY})
+      this.setState({img: "charTop"})
+    }
+  }
+  componentDidMount(){
+    document.addEventListener("keydown", this.goTop, false);
+    document.addEventListener("keydown", this.goBottom, false);
+    document.addEventListener("keydown", this.goRight, false);
+    document.addEventListener("keydown", this.goLeft, false);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.goTop, false);
+    document.removeEventListener("keydown", this.goBottom, false);
+    document.removeEventListener("keydown", this.goRight, false);
+    document.removeEventListener("keydown", this.goLeft, false);
+  }
   render() {
     return (
       <div className="App">
@@ -41,8 +76,8 @@ class App extends Component {
             <tr key={rowIndex}>
               {row.map((tileId, colIndex) => 
                 <th key={colIndex}>
-                  <div className="tile">
-                    <img className="tileset" src={"./assets/tiles/"+ tileId +".png"} alt="tile"/>
+                  <div className="tile" style={{ backgroundImage: `url(${"./assets/tiles/" + tileId +".png"})` }}>
+            
                       {
                         (rowIndex === this.state.posY && colIndex === this.state.posX)?
                         <img className="character" src={"./assets/characters/"+ this.state.img +".png"} alt="character"/>
@@ -56,8 +91,6 @@ class App extends Component {
           ))
           }
         </table>
-        <button onClick={() => this.goLeft()}>Left</button>
-        <button onClick={() => this.goRight()}>Right</button>
       </div>
     );
   }
