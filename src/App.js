37be6eba711de3946ead8e17da1421eaf88df535
JeanCarrillo <jeancarrillo@gmail.com>
2019-04-06 14:07:00 +0200

    refactor, move delay prototype, to do components

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ class App extends Component {
   }
 }
 
-
 class Game extends Component {
   render() {
     const labyrinths = require('./labyrinths.json');
     const labyrinth = labyrinths.labyrinth1
     return (
       <div className="Game">
+{/*    TO DO
+        <Chrono />
+        <Capacities />
+*/}
         <Board labyrinth={labyrinth} />
         <Player labyrinth={labyrinth} />
       </div>
@@ -61,7 +64,8 @@ class Tile extends Component {
 class Player extends Component {
   constructor(props) {
     super(props)
-    this.move = this.move.bind(this);
+    this.action = this.action.bind(this);
+    this.canMove = true;
     this.state = {
       posX: 1,
       posY: 0,
@@ -75,46 +79,57 @@ class Player extends Component {
       return false
     return true
   }
-  move(event) {
-    // Move right
-    if (event.keyCode === 39) {
-      this.setState({ img: "charRight" })
-      if (this.state.posX + 1 < this.props.labyrinth[this.state.posY].length && this.checkTile(1, 0)) {
-        const posX = this.state.posX + 1
-        this.setState({ posX })
-      }
+  action(event) {
+    // MOVES
+    if (this.canMove && (event.keyCode === 39 || event.keyCode === 37 || event.keyCode === 40 || event.keyCode === 38)) {
+        this.canMove = false
+        // Move right
+        if (event.keyCode === 39) {
+          this.setState({ img: "charRight" })
+          if (this.state.posX + 1 < this.props.labyrinth[this.state.posY].length && this.checkTile(1, 0)) {
+            const posX = this.state.posX + 1
+            this.setState({ posX })
+          }
+        }
+        // Move left
+        if (event.keyCode === 37) {
+          this.setState({ img: "charLeft" })
+          if (this.state.posX > 0 && this.checkTile(-1, 0)) {
+            const posX = this.state.posX - 1
+            this.setState({ posX })
+          }
+        }
+        // Move down
+        if (event.keyCode === 40) {
+          this.setState({ img: "charBottom" })
+          if (this.state.posY + 1 < this.props.labyrinth.length && this.checkTile(0, 1)) {
+            const posY = this.state.posY + 1
+            this.setState({ posY })
+          }
+        }
+        // Move up
+        if (event.keyCode === 38) {
+          this.setState({ img: "charTop" })
+          if (this.state.posY > 0 && this.checkTile(0, -1)) {
+            const posY = this.state.posY - 1
+            this.setState({ posY })
+          }
+        }
+      // Move delay value
+        setTimeout(() => { this.canMove = true
+      }, 100)
     }
-    // Move left
-    if (event.keyCode === 37) {
-      this.setState({ img: "charLeft" })
-      if (this.state.posX > 0 && this.checkTile(-1, 0)) {
-        const posX = this.state.posX - 1
-        this.setState({ posX })
-      }
-    }
-    // Move down
-    if (event.keyCode === 40) {
-      this.setState({ img: "charBottom" })
-      if (this.state.posY + 1 < this.props.labyrinth.length && this.checkTile(0, 1)) {
-        const posY = this.state.posY + 1
-        this.setState({ posY })
-      }
-    }
-    // Move up
-    if (event.keyCode === 38) {
-      this.setState({ img: "charTop" })
-      if (this.state.posY > 0 && this.checkTile(0, -1)) {
-        const posY = this.state.posY - 1
-        this.setState({ posY })
-      }
-    }
+    // To do :
+    // Activate abilities
+
   }
-  
+
+
   componentDidMount() {
-    document.addEventListener("keydown", this.move, false)
+    document.addEventListener("keydown", this.action, false)
   }
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.move, false)
+    document.removeEventListener("keydown", this.action, false)
   }
 
   render() {
@@ -127,7 +142,7 @@ class Player extends Component {
       backgroundRepeat: "no-repeat",
       height: "48px",
       width: "48px",
-      transitionDuration: "0.5s",
+      transitionDuration: "500ms",
       // To do: cleaner calculation
       top: this.state.posY * this.state.pixelsPerTile + 'px',
       left: 11 + this.state.posX * this.state.pixelsPerTile + 'px'
@@ -138,6 +153,35 @@ class Player extends Component {
     );
   }
 }
-
-
+/*          TO DO
+class Chrono extends Component {
+  render() {
+    return (
+      <div className="Chrono">
+        <p>Chronomètre</p>
+      </div>
+    );
+  }
+}
+class Capacities extends Component {
+  render() {
+    return (
+      <div className="Capacities">
+        <Capacity element="water" />
+        <Capacity element="fire" />
+      </div>
+    );
+  }
+}
+class Capacity extends Component {
+  render() {
+    return (
+      <div
+        className="capacityStyle"
+        style={{ backgroundImage: `url(${"./assets/capacities/" + this.props.element + ".png"})` }}
+      />
+    );
+  }
+}
+*/
 export default App;
